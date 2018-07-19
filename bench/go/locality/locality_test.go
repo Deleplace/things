5c928b15da4568bdd776ead7b3d65560f1b77a4d
Ripounet <rip0320@yahoo.com>
2018-07-19 16:53:55 +0200

    Benchmark: memory locality. Predictible access is faster.

--- bench/go/locality/locality_test.go
+++ bench/go/locality/locality_test.go
@@ -5,10 +5,13 @@ import (
 	"testing"
 )
 
+const M = 100000
+
+// How fast is it to access a[0], a[1], ..., a[M-1] ?
+// (thru an indirection array)
 func BenchmarkAccessForward(b *testing.B) {
 	rand.Seed(42)
 
-	M := 100000
 	a := make([]uint, M)
 	index := make([]int, M)
 	for i := range a {
@@ -28,10 +31,11 @@ func BenchmarkAccessForward(b *testing.B) {
 	}
 }
 
+// How fast is it to access a[M-1], a[M-2], ..., a[0] ?
+// (thru an indirection array)
 func BenchmarkAccessBackward(b *testing.B) {
 	rand.Seed(42)
 
-	M := 100000
 	a := make([]uint, M)
 	index := make([]int, M)
 	for i := range a {
@@ -51,6 +55,8 @@ func BenchmarkAccessBackward(b *testing.B) {
 	}
 }
 
+// How fast is it to access all elements of a in random order ?
+// (thru an indirection array)
 func BenchmarkAccessRandom(b *testing.B) {
 	rand.Seed(42)
 
